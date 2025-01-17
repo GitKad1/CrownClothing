eb3f39f16d7c9ff0dd18a87e5f4f8092176c7d3e
Kadmiel Herbert <kadmieleherbert@gmail.com>
2025-01-17 18:56:30 -0400

    first commit

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,29 @@
+import { Name, Price, Footer, ProductCardContainer } from "./product-card.styles";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
+  return (
+    <ProductCardContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+        onClick={addProductToCart}
+      >
+        Add to Cart
+      </Button>
+    </ProductCardContainer>
+  );
+};
+
+export default ProductCard;
